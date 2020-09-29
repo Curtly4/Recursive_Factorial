b299525c4439e67d5c02a4238568be8275e1a4e3
Curtly Jack <ballislife_burt@Curtlys-MacBook-Pro.local>
2020-09-28 21:27:28 -0400

    Initial commit

---
+++ src/fact.java
@@ -0,0 +1,11 @@
+public class fact {
+    public static long fact(int n){
+        if(n==0 || n == 1){
+            return 1;
+        }
+        return fact(n-1) * n;
+    }
+    public static void main(String[] args){
+        System.out.println(fact(5));
+    }
+}
